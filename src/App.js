c1fe6e728a4f8653f9c563bbc2f9069313424971
nuno palomino <nunopalomino@gmail.com>
2023-02-11 02:12:05 -0300

    added stages and new functions to the game

--- src/App.js
+++ src/App.js
@@ -14,20 +14,49 @@ import Game from './components/Game';
 import GameOver from './components/GameOver';
 
 const stages = [
-  {id: 1, name: 'Start'},
-  {id: 2, name: 'Game'},
-  {id: 3, name: 'End'},
+  { id: 1, name: 'Start' },
+  { id: 2, name: 'Game' },
+  { id: 3, name: 'End' },
 ];
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState('');
+  const [pickedCategory, setPickedCategory] = useState('');
+  const [letters, setLetters] = useState([]);
+
+  const pickWordAndCategory = () => {
+    //  pick a random category
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * Object.keys(categories).length)];
+
+    // pick a random word
+    const word = words[category][Math.floor(Math.random() * words[category].length)];
+
+    return { word, category };
+  };
 
   // Starts the secret word game
 
   const startGame = () => {
+    // pick word and pick category
+    const { word, category } = pickWordAndCategory();
     setGameStage(stages[1].name);
+    // create and array of letters
+    let wordLetters = word.split('');
+
+    wordLetters = wordLetters.map((l) => l.toLowerCase());
+
+    console.log(word, category);
+    console.log(wordLetters);
+
+    // fill states 
+    setPickedWord(word);
+    setPickedCategory(category);
+    setLetters(wordLetters);
+
   };
 
   // Process the letter input
@@ -41,8 +70,6 @@ function App() {
   const retry = () => {
     setGameStage(stages[0].name);
   }
-
-
 
   return (
     <div className="App">
