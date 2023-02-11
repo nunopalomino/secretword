995c7be361c30246732d9de9b0d53e7e18db874a
nuno palomino <nunopalomino@gmail.com>
2023-02-11 02:12:54 -0300

    added new text to the start screen

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -6,7 +6,7 @@ const StartScreen = ({ startGame }) => {
       <h1>
         Secret Word
       </h1>
-      <p>Click the start button to start playing</p>
+      <p>Click the start button to start playing!</p>
       <button onClick={startGame}>Start</button>
     </div>
   )
